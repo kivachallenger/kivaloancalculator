bb64e40545b4a9a1811ada5de782633d421b50ab
Sean Bae (Kyung Mok Bae) <s@seanbae.net>
2015-04-18 20:33:49 -0400

    font awesome

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,22 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KIVA Impact Calculator' });
+  res.render('index', { title: 'KIVA Impact Calculator', lender: "ERROR" });
 });
 
 router.get('/*', function(req, res, next) {
 
   request.get("http://api.kivaws.org/v1/teams/search.json?sort_by=member_count&q=" + req.params[0], function(err, response, body) {
     if (err) {
-      res.render('index', { title: 'KIVA Impact Calculator', lender: "ERROR", name: "ERROR" });
+      res.render('index', { title: 'KIVA Impact Calculator', lender: "ERROR", name: "ERROR", query_message: "ERROR: FAILED TO MAKE KIVA API CALL" });
     }
 
     var lenderID = JSON.parse(body);
 
-    if (lenderID["teams"].length == 0) {
-      res.render('index', { title: 'KIVA Impact Calculator', lender: "ERROR", name: "ERROR" });
+    if (typeof lenderID["teams"] == 'undefined') {
+      res.render('index', { title: 'KIVA Impact Calculator', lender: "ERROR", name: "ERROR", query_message: "ERROR: FAILED TO MAKE KIVA API CALL" });
+    } else if (lenderID["teams"].length == 0) {
+      res.render('index', { title: 'KIVA Impact Calculator', lender: "ERROR", name: "ERROR", query_message: "ERROR: QUERY IS INVALID" });
     } else {
       res.render('index', { title: 'KIVA Impact Calculator', lender: lenderID["teams"][0]["id"], name: lenderID["teams"][0]["name"] });
     }
